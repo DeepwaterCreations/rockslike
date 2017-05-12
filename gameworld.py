117cb1c6489b326aa0637b9f241755d54623a7e5
Bismuth the Chatoyant <bismuth@mineralia.club>
2017-05-12 03:07:43 +0000

    Keep list of entities and add entity chars to matrix

--- gameworld.py
+++ gameworld.py
@@ -7,6 +7,16 @@ class MapFeature():
     def get_char(self):
         return self.char
 
+class Entity():
+    """A dynamic object on the map, such as a player or monster"""
+
+    def __init__(self, char, x, y):
+        self.char = char
+        self.x = x
+        self.y = y
+
+    def get_char(self):
+        return self.char
 
 class GameWorld():
     """A class to hold the current state of the game world"""
@@ -25,6 +35,20 @@ class GameWorld():
         #that are located in that tile.
         self._world = [[[self.map_features["floor"]] for x in range(self.width)] for y in range(self.height)]
 
+        #add player to the map
+        player = Entity('@', self.width//2, self.height//2)
+        self._entities = [player]
+        
+    def _flatten_map(self):
+        """Returns a matrix where each cell holds the char of the top game object at that location"""
+        flattened = [['' for x in range(self.width)] for y in range(self.height)]
+        for y, row in enumerate(self._world):
+            for x, tile in enumerate(row):
+                flattened[y][x] = tile[len(tile) - 1].char
+        for entity in self._entities:
+            flattened[entity.y][entity.x] = entity.char
+        return flattened
+
     def get(self, x, y):
         """Returns the contents of the tile at x, y"""
         return self._world[y][x]
@@ -37,10 +61,10 @@ class GameWorld():
         """Return the top-level tile characters for every tile"""
         #TODO: This won't really work if we want colors.
         worldstring = ""
-        for row in self._world:
+        flattened = self._flatten_map()
+        for row in flattened:
             rowstring = ""
-            for tile in row:
-                char = tile[len(tile)-1].get_char()
+            for char in row:
                 rowstring += char
             worldstring += rowstring
             worldstring += '\n'
