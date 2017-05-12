4a3eb3522b8388c912c27d9aefb98afb5ceccaf5
Bismuth the Chatoyant <bismuth@mineralia.club>
2017-05-12 02:25:51 +0000

    Add a class to hold gameworld state

---
+++ gameworld.py
@@ -0,0 +1,45 @@
+class MapFeature():
+    """A static element of the map, such as a floor or wall tile"""
+
+    def __init__(self, char):
+        self.char = char
+
+    def get_char(self):
+        return self.char
+
+
+class GameWorld():
+    """A class to hold the current state of the game world"""
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.map_features = {
+                "empty": MapFeature(' '),
+                "floor": MapFeature('.'),
+                "wall": MapFeature('#')
+                }
+        #World is represented by a 2d matrix of lists
+        #Each coordinate in the matrix is a tile
+        #The list for that tile holds game objects
+        #that are located in that tile.
+        self.world = [[[self.map_features["empty"]] for x in range(self.width)] for y in range(self.height)]
+
+    def update_world(self):
+        """Generate the results of a single turn"""
+        pass
+
+
+    def as_string(self):
+        """Return the top-level tile characters for every tile"""
+        #TODO: This won't really work if we want colors.
+        worldstring = ""
+        for row in self.world:
+            rowstring = ""
+            for tile in row:
+                char = tile[0].get_char()
+                rowstring += char
+            worldstring += rowstring
+            worldstring += '\n'
+        return worldstring
+
