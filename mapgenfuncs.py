28bc2275fd996366e918b4ac8a5e0b11a6ac6c36
Bismuth the Chatoyant <bismuth@mineralia.club>
2017-05-24 02:57:36 +0000

    Add empty box mapgenfunc

--- mapgenfuncs.py
+++ mapgenfuncs.py
@@ -1,4 +1,6 @@
 import parsemap
+import entities
+import mapfeatures
 
 ### MAP GENERATION FUNCTIONS ###
 #Take a gameworld object plus other args, return world and map_entities,
@@ -11,3 +13,19 @@ def load_from_file(gw, mapfile):
     world, entities_partials = parsemap.parse_file(mapfile)
     map_entities = [partial(get_gameworld_cell=gw.get) for partial in entities_partials]
     return world, map_entities
+
+def empty_box(gw, width, height):
+    """Generate a big, empty box of floor with walls around it and put the player in the middle"""
+    world = [[[mapfeatures.Wall()] for x in range(width+2)]]
+    floor_row = [[mapfeatures.Wall()]]
+    floor_row += [[mapfeatures.Floor()] for x in range(width)]
+    floor_row += [[mapfeatures.Wall()]]
+    for y in range(height):
+        world.append(floor_row.copy())
+    world.append([[mapfeatures.Wall()] for x in range(width+2)])
+
+    player = entities.Player(width//2, height//2, gw.get)
+    map_entities = [player]
+
+    return world, map_entities
+
