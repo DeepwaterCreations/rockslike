2d5d5ff6aa9a3a9d9d6aac7c8fd2d5213ab198d6
Bismuth the Chatoyant <bismuth@mineralia.club>
2017-07-22 06:05:30 +0000

    Overhaul map parsing for increased clarity + simplicity
    I love the feeling when you destroy a bunch of terrible code and replace
    it with a smaller amount of better code. Though I'm sure there's still
    work to be done in this file.

--- parsemap.py
+++ parsemap.py
@@ -16,103 +16,73 @@ __map_dict = {
         }
 
 def parse_file(map_file):
-    """Divide file into map features section and entities section
+    """Divide file into map art section and JSON section, which are separated by a newline
 
-    Sections are delimited by blank lines
     Map Art: An ASCII picture of the map
-    Map Details: A JSON object with metadata about the map
-            version - the parser version to use (not currently observed)
-            player_spawn_x - the x coordinate where the player should spawn in
-            player_spawn_y - the y coordinate where the player should spawn in
-    Map Features: A JSON object with coordinate pairs keyed to lists containing objects with these fields:
-            classname - the name of a subclass of MapFeature that should be generated for this tile
-            fgcolor, bgcolor - strings describing the foreground and background color of
-                the tile
-            args - a list of arguments to pass to the object's constructor
-            kwargs - a dict of keyword arguments to pass to the object's constructor
-        All of these fields are optional for MapFeatures. If omitted, they will
-        be inferred from the ASCII art. Additionally, MapFeatures displayed in the ASCII art will be
-        created even if there is no associated listing in the object.
-    Entities: A JSON list of objects with these fields:
-            classname - the name of a subclass of Entity that describes this entity
-            x_coord - the x coordinate of the entity's position
-            y_coord - the y coordinate of the entity's position
-            tilechar - the character to display for the object
-            fgcolor, bgcolor - strings describing the foreground and backgrounc color
-                of the tile
-            args - (optional) a list of arguments to pass to the entity's constructor
-            ksargs - (optional) a dict of keyword arguments to pass to the entity's constructor
+    JSON: a JSON object optionally containing some or all of the following:
+        "mapdetails": A JSON object with metadata about the map
+                version - the parser version to use (not currently observed)
+                player_spawn_x - the x coordinate where the player should spawn in
+                player_spawn_y - the y coordinate where the player should spawn in
+        "mapfeatures": A JSON object with coordinate pairs keyed to lists containing objects with these fields:
+                classname - the name of a subclass of MapFeature that should be generated for this tile
+                fgcolor, bgcolor - strings describing the foreground and background color of
+                    the tile
+                args - a list of arguments to pass to the object's constructor
+                kwargs - a dict of keyword arguments to pass to the object's constructor
+            All of these fields are optional for MapFeatures. If omitted, they will
+            be inferred from the ASCII art. Additionally, MapFeatures displayed in the ASCII art will be
+            created even if there is no associated listing in the object.
+        "entities": A JSON list of objects with these fields:
+                classname - the name of a subclass of Entity that describes this entity
+                x_coord - the x coordinate of the entity's position
+                y_coord - the y coordinate of the entity's position
+                tilechar - the character to display for the object
+                fgcolor, bgcolor - strings describing the foreground and backgrounc color
+                    of the tile
+                args - (optional) a list of arguments to pass to the entity's constructor
+                ksargs - (optional) a dict of keyword arguments to pass to the entity's constructor
     """
-    mapdetails_delimiter = "((MAPDETAILS))"
-    mapfeatures_delimiter = "((MAPFEATURES))"
-    entities_delimiter = "((ENTITIES))"
+    #Read the file, strip leading and trailing whitespace, and break into a list of strings
+    #by line
+    file_text = map_file.read()
+    file_text = file_text.strip()
+    file_text_lines = file_text.splitlines()
 
-    map_text = map_file.read()
-    map_text = map_text.strip()
-    map_text = map_text.splitlines()
+    #Separate the file into its component chunks
+    mapart_end_index = file_text_lines.index("") if "" in file_text_lines else len(file_text_lines)
+    mapart = file_text_lines[:mapart_end_index]
 
-    #Break the file into its component chunks
-    mapart_end_index = map_text.index("") if "" in map_text else len(map_text)
-    mapart_text = map_text[:mapart_end_index]
-    json_text = map_text[mapart_end_index:]
-    
-    mapdetails_text = None
-    mapfeatures_text = None
-    entities_text = None
-    if mapdetails_delimiter in json_text:
-        mapdetails_start_index = json_text.index(mapdetails_delimiter)+1
-        if mapfeatures_delimiter in json_text:
-            mapdetails_end_index = json_text.index(mapfeatures_delimiter)
-        elif entities_delimiter in json_text:
-            mapdetails_end_index = json_text.index(entities_delimiter)
-        else:
-            mapdetails_end_index = len(json_text)
-        mapdetails_text = json_text[mapdetails_start_index:mapdetails_end_index]
+    json_text = file_text_lines[mapart_end_index:] if mapart_end_index < len(file_text_lines) else ""
+    json_text = "".join(json_text) #Put JSON lines back together into a single string so JSON can be parsed
+    map_obj = json.loads(json_text) if len(json_text) > 0 else {}
 
-    if mapfeatures_delimiter in json_text:
-        mapfeatures_start_index = json_text.index(mapfeatures_delimiter)+1
-        mapfeatures_end_index = json_text.index(entities_delimiter) if entities_delimiter in json_text \
-                else len(json_text)
-        mapfeatures_text = json_text[mapfeatures_start_index:mapfeatures_end_index]
+    #Get members of JSON objects
+    mapdetails = map_obj["mapdetails"] if "mapdetails" in map_obj else {}
+    mapfeatures_by_coord = map_obj["mapfeatures"] if "mapfeatures" in map_obj else {}
+    entities_list = map_obj["entities"] if "entities" in map_obj else []
 
-    if entities_delimiter in json_text:
-        entities_start_index = json_text.index(entities_delimiter)+1
-        entities_end_index = len(json_text)
-        entities_text = json_text[entities_start_index:entities_end_index]
+    #Parse mapfeature and entity data to create actual game objects
+    mapfeatures = __parse_map_features(mapart, mapfeatures_by_coord)
+    entities = __parse_entities(entities_list)
 
-    map_details = __parse_map_details(mapdetails_text)
-    matrix = __parse_map_features(mapart_text, mapfeatures_text)
-    map_entities = __parse_entities(entities_text)
-
-    if "player_spawn_x" in map_details and "player_spawn_y" in map_details:
-        player_spawn = (map_details["player_spawn_x"], map_details["player_spawn_y"])
+    #Pull player spawn info from mapdetails
+    if "player_spawn_x" in mapdetails and "player_spawn_y" in mapdetails:
+        player_spawn = (mapdetails["player_spawn_x"], mapdetails["player_spawn_y"])
     else:
         player_spawn = (0,0)
-    return (matrix, map_entities, player_spawn)
 
-def __parse_map_details(mapdetails_text):
-    """Read map metadata"""
-    if mapdetails_text is None:
-        return {}
+    return (mapfeatures, entities, player_spawn)
 
-    mapdetails_text = "".join(mapdetails_text)
-    json_mapdetails = json.loads(mapdetails_text)
-    return json_mapdetails
-
-def __parse_map_features(mapart_text, mapfeatures_text):
+def __parse_map_features(mapart, mapfeatures_by_coord):
     """Read a description of a map from a file and generate a matrix of map features"""
-    matrix = []
-    if mapfeatures_text is not None:
-        mapfeatures_text = "".join(mapfeatures_text)
-        json_mapfeatures = json.loads(mapfeatures_text)
-    else:
-        json_mapfeatures = {}
+    mapfeatures_matrix = []
 
-    for y, line in enumerate(mapart_text):
+    for y, line in enumerate(mapart):
         parsed_line = []
         for x, cell in enumerate(line):
-            if "{0},{1}".format(x, y) in json_mapfeatures:
-                obj = json_mapfeatures["{0},{1}".format(x, y)]
+            if "{0},{1}".format(x, y) in mapfeatures_by_coord:
+                obj = mapfeatures_by_coord["{0},{1}".format(x, y)]
                 if "classname" in obj:
                     mapfeature_class = getattr(sys.modules["mapfeatures"], obj["classname"])
                 elif cell in __map_dict:
@@ -133,31 +103,26 @@ def __parse_map_features(mapart_text, mapfeatures_text):
             else:
                 feature = __map_dict[cell]() if cell in __map_dict else mapfeatures.Void()
             parsed_line.append(feature)
-        matrix.append(parsed_line)
-    return matrix
+        mapfeatures_matrix.append(parsed_line)
+    return mapfeatures_matrix
 
-def __parse_entities(entities_text):
+def __parse_entities(json_entities):
     """Build a list of callable game object constructors from a list of JSON objects"""
-    if entities_text is None:
-        return []
 
-    entities_text = "".join(entities_text)
-    json_entities = json.loads(entities_text)
-
-    map_entities = []
-    for obj in json_entities:
-        x_coord = int(obj['x_coord'])
-        y_coord = int(obj['y_coord'])
-        fgcolor = obj['fgcolor'] if 'fgcolor' in obj else "WHITE"
-        bgcolor = obj['bgcolor'] if 'bgcolor' in obj else "BLACK"
-        tile = Tile(obj['tilechar'], fgcolor, bgcolor) if 'tilechar' in obj else None
-        args = obj['args'] if 'args' in obj else []
+    parsed_entities = []
+    for entity_obj in json_entities:
+        x_coord = int(entity_obj['x_coord'])
+        y_coord = int(entity_obj['y_coord'])
+        fgcolor = entity_obj['fgcolor'] if 'fgcolor' in entity_obj else "WHITE"
+        bgcolor = entity_obj['bgcolor'] if 'bgcolor' in entity_obj else "BLACK"
+        tile = Tile(entity_obj['tilechar'], fgcolor, bgcolor) if 'tilechar' in entity_obj else None
+        args = entity_obj['args'] if 'args' in entity_obj else []
         if tile is not None:
             args.append(tile)
         args.append(x_coord)
         args.append(y_coord)
-        kwargs = obj['kwargs'] if 'kwargs' in obj else {}
-        entity_class = getattr(sys.modules["entities"], obj['classname'])
+        kwargs = entity_obj['kwargs'] if 'kwargs' in entity_obj else {}
+        entity_class = getattr(sys.modules["entities"], entity_obj['classname'])
         entity_partial = functools.partial(entity_class, *args, **kwargs)
-        map_entities.append(entity_partial)
-    return map_entities
+        parsed_entities.append(entity_partial)
+    return parsed_entities
